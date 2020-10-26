5215328aeca0e8988124fa19db8c08569454b41e
Aren S <adsalmela@gmail.com>
2020-10-26 17:59:29 -0400

    Input and button work. City result is displayed in console.

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
 
- var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=input.formcontrol&appid=956c1c0d82794f489f9f91ee32d9b51f"
+$("#find-city").on("click", function(event){
+    event.preventDefault();
+    let city = $("#city-search").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=956c1c0d82794f489f9f91ee32d9b51f"
+   
+    $.ajax({
+       url: queryURL,
+       method: "GET"
+    })
+   
+     .then(function(response) {
+        console.log(queryURL)
+        console.log(response)
+    
+    }) 
 
- $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+
+})
+
+ 
 
   
-  $("#city-search")
-  
-  .then(function(response) {
-    console.log(queryURL)
-    console.log(response)
+ 
+    
 
-  })
+  
+  
+ 
 
 
 
