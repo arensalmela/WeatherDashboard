c31dbb74d510754a668a15bde70c70be64cecb88
Aren S <adsalmela@gmail.com>
2020-10-29 18:21:56 -0400

    UV function is working and appending to the HTML.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+  let cityInput = $("#city-search");
   $("#find-city").on("click", function (event) {
     event.preventDefault();
-    let city = $("#city-search").val();
-    $("#city-search").val("");
+    let city = cityInput.val();
+    cityInput.val("");
     findWeather(city);
     forecast(city);
   });
@@ -24,6 +25,7 @@ $(document).ready(function () {
             JSON.stringify(searchHistory)
           );
           previousSearch(searchHistory);
+          $("#today").val = "";
         }
 
         console.log(response);
@@ -40,7 +42,6 @@ $(document).ready(function () {
         let cityWind = $("<p>")
           .addClass("card-text")
           .text("Wind: " + response.wind.speed + " MPH");
-        //let cityUv =
 
         cardBody.append(cityName);
         cardBody.append(cityTemp);
@@ -49,12 +50,34 @@ $(document).ready(function () {
         card.append(cardBody);
         $("#today").append(card);
 
+        getUv(response);
+
+        //getUv(lat, lon);
         //$(".city").text(cityName);
         //$(".temp").text("Temperature: " + parseInt(cityTemp) + " ℉");
         //$(".humidity").text("Humidity: " + cityHumid + " %");
         //$(".wind").text("Wind Speed: " + cityWind + " MPH");
         //$(".uvIndex").text(cityUv);
       },
+    });
+  }
+
+  function getUv(response) {
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var indexUrl =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=bf07b2cbcf62e6d7ae2b1536e215c869";
+
+    $.ajax({
+      url: indexUrl,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response, "UV index");
+      $("#uvIndex").text("UV Index : " + response.value);
     });
   }
 
@@ -70,19 +93,20 @@ $(document).ready(function () {
       success: function (forecast) {
         console.log(forecast);
         let list = forecast.list;
-        //$("#forecast").html("");
 
         for (let i = 39; i >= 0; i = i - 8) {
           var temp = ((list[i].main.temp - 273.15) * 1.8 + 32).toFixed(2);
           var iconId = list[i].weather[0].icon;
           var humidity = list[i].main.humidity;
-          var date = list[i].dt_txt;
+          //var date = list[i].dt_txt;
 
           //Commented these out as it was causing error
           //var day = date.getDate();
           //var month = date.getMonth();
           //var year = date.getFullYear();
-
+          var day = "29";
+          var month = "10";
+          var year = "2020";
           var formatedDate = `${month + 1}/${day}/${year}`;
 
           var col = $("<div>");
@@ -106,19 +130,12 @@ $(document).ready(function () {
           myCard.append(weatherPic);
           myCard.append(pTag2);
           myCard.append(pTag3);
-
+          console.log(myCard);
           $("forecastBlocks").prepend(col);
         }
       },
     });
   }
-
-  //   $.ajax({
-  //     // UVindex
-  //     url: forecastURL,
-  //     method: "GET",
-  //     success: function (UVindex) {},
-  //   });
 
   function previousSearch(name) {
     var cityList = $("<li>").text(name);
